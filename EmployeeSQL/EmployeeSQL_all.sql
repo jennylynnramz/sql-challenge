305a3c17481502a907e7ed79380ef98d6a0ea87e
Jennifer Ramsey <60010423+jennylynnramz@users.noreply.github.com>
2020-04-30 22:07:45 -0700

    completed SQL, added epilogue. Added ipnyb file to begin visualization

---
+++ EmployeeSQL/EmployeeSQL_all.sql
@@ -0,0 +1,148 @@
+drop table if exists department_manager;
+drop table if exists department_employees;
+drop table if exists departments;
+
+CREATE TABLE departments 
+	(dept_no VARCHAR PRIMARY KEY,
+	dept_name VARCHAR);
+	
+CREATE TABLE department_employees
+	(emp_no VARCHAR,
+	dept_no VARCHAR,
+	FOREIGN KEY (dept_no) REFERENCES departments(dept_no));
+	
+CREATE TABLE department_manager
+	(dept_no VARCHAR,
+	emp_no VARCHAR,
+	FOREIGN KEY (dept_no) REFERENCES departments(dept_no)
+	);
+
+drop table if exists titles;
+drop table if exists salaries;
+drop table if exists employees;
+
+CREATE TABLE employees (
+	emp_no VARCHAR PRIMARY KEY,
+	emp_title_id VARCHAR,
+	birth_date DATE,
+	first_name VARCHAR,
+	last_name VARCHAR,
+	sex VARCHAR(1),
+	hire_date DATE
+	);
+	
+CREATE TABLE salaries (
+	emp_no VARCHAR,
+	salary INT,
+	FOREIGN KEY (emp_no) REFERENCES employees(emp_no)
+);
+
+CREATE TABLE titles (
+	title_id VARCHAR PRIMARY KEY,
+	title VARCHAR
+);
+
+---------------------------------------------
+SELECT * FROM department_employees;
+SELECT * FROM department_manager;
+SELECT * FROM departments;
+SELECT * FROM employees;
+SELECT * FROM salaries;
+SELECT * FROM titles;
+
+--List the following details of each employee: employee number, last name, first name, sex, and salary.
+SELECT 
+	employees.emp_no,
+	employees.last_name,
+	employees.first_name,
+	employees.sex,
+	salaries.salary
+FROM employees
+JOIN salaries
+ON employees.emp_no = salaries.emp_no;
+
+--List first name, last name, and hire date for employees who were hired in 1986.
+
+SELECT
+	first_name,
+	last_name,
+	hire_date
+FROM employees
+WHERE hire_date BETWEEN '1986-01-01' AND '1986-12-31'
+ORDER BY 3;
+
+--List the manager of each department with the following information: department number, department name, the manager's employee number, last name, first name.
+
+SELECT 
+	departments.dept_no,
+	departments.dept_name,
+	department_manager.emp_no,
+	employees.last_name,
+	employees.first_name
+FROM departments
+JOIN department_manager ON departments.dept_no = department_manager.dept_no
+JOIN employees ON department_manager.emp_no = employees.emp_no;
+	
+--List the department of each employee with the following information: employee number, last name, first name, and department name.
+SELECT
+	department_manager.emp_no,
+	employees.last_name,
+	employees.first_name,
+	department_manager.dept_no
+FROM department_manager
+JOIN employees
+ON department_manager.emp_no = employees.emp_no;
+
+--List first name, last name, and sex for employees whose first name is "Hercules" and last names begin with "B."
+
+SELECT
+	first_name,
+	last_name,
+	sex
+FROM employees
+WHERE first_name = 'Hercules'
+	AND last_name LIKE 'B%';
+
+--List all employees in the Sales department, including their employee number, last name, first name, and department name.
+
+SELECT
+	employees.emp_no,
+	employees.last_name,
+	employees.first_name,
+	departments.dept_name
+FROM employees
+JOIN department_employees ON department_employees.emp_no = employees.emp_no
+JOIN departments ON department_employees.dept_no = departments.dept_no;
+
+--List all employees in the Sales and Development departments, including their employee number, last name, first name, and department name.
+
+SELECT
+	employees.emp_no,
+	employees.last_name,
+	employees.first_name,
+	departments.dept_name
+FROM employees
+JOIN department_employees ON employees.emp_no = department_employees.emp_no
+JOIN departments ON department_employees.dept_no = departments.dept_no
+WHERE departments.dept_name = 'Sales'
+	OR departments.dept_name = 'Development'
+ORDER BY dept_name;
+
+--In descending order, list the frequency count of employee last names, i.e., how many employees share each last name.
+SELECT 
+	last_name,
+	COUNT(last_name)
+FROM employees
+GROUP BY 1
+ORDER BY 2 DESC;
+	
+
+
+--epilogue.
+
+SELECT *
+FROM employees
+WHERE employees.emp_no = '499942'
+;
+
+
